ddb2bdcb73039b045794d01213c93b7f717c2071
ksimali <cassim.ali@outlook.fr>
2024-02-22 08:38:13 -0500

    Add comment & Increase code readability with

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ public class Main extends JFrame { /* extends JFrame class pour utiliser les met
     // Créer un panel pour ajouter des boutons
     static JPanel panel = new JPanel();
     public Main(){
+
         // création de la fenetre principale
         setTitle("Financement Auto");
         setSize(650, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // centre la fenêtre au centre de l'écran
 
-        // Creation des boutons Se connecter et S'enregistrer
+        // Creation des boutons 'Se connecter' et 'S'enregistrer'
         JButton loginButton = new JButton("Se connecter");
         JButton registerButton = new JButton("S'enregistrer");
+
         // Ajout des boutons au panel via la methode add()
         panel.add(loginButton);
         panel.add(registerButton);
