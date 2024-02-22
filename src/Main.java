f0db24ea6d1fd2de1709b1e33ad98d7d5bfc204c
ksimali <cassim.ali@outlook.fr>
2024-02-21 23:42:59 -0500

    Ajout des boutons -Création du panel pour acceuillir les boutons -Créations bouton "Se connecter" -Création bouton "S'enregistrer" -Ajouter boutons au panel -Ajout du panel au Frame

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,29 @@ import javax.swing.*;
 
 public class Main extends JFrame { /* extends JFrame class pour utiliser les methode de la classe */
 
+    // Créer un panel pour ajouter des boutons
+    static JPanel panel = new JPanel();
     public Main(){
+        // création de la fenetre principale
         setTitle("Financement Auto");
         setSize(650, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(null); // centre la fenêtre au centre de l'écran
+
+        // Creation des boutons Se connecter et S'enregistrer
+        JButton loginButton = new JButton("Se connecter");
+        JButton registerButton = new JButton("S'enregistrer");
+        // Ajout des boutons au panel via la methode add()
+        panel.add(loginButton);
+        panel.add(registerButton);
+
     }
     public static void main(String[] args) {
 
         Main frame = new Main();
+        //Ajout du panel au Frame
+        frame.add(panel);
+        // rendre visible le Frame
         frame.setVisible(true);
 
     }
