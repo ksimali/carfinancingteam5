4654c59043b4c8cdad4a2ba9f51a3222f02d1ac6
ksimali <cassim.ali@outlook.fr>
2024-02-24 16:36:17 -0500

    Refactoring Main.java files: - seperate Main Class attributes, constructor and method

--- src/Main.java
+++ src/Main.java
@@ -6,47 +6,78 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Main extends JFrame {
-    private CardLayout cardLayout = new CardLayout();
-    private JPanel cardPanel = new JPanel(cardLayout);
-    private LoginView loginView = new LoginView();
-    private RegisterView registerView = new RegisterView();
+public class Main {
+    // class attributes
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JPanel descriptionPanel;
+    private JButton loginButton;
+    private JButton registerButton;
+    private  JLabel descriptionLabel;
+    private JLabel contactLabel;
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    private LoginView loginView;
+    private RegisterView registerView;
 
-    public Main() {
+    // constructor for the Main class
+    public Main(){
+        frame = new JFrame();
+        mainPanel = new JPanel();
+        descriptionPanel = new JPanel();
+        loginButton = new JButton("Connexion");
+        registerButton = new JButton("Inscription");
+        descriptionLabel = new JLabel("Financement de votre futur véhicule à un taux inférieur à 5%");
+        contactLabel = new JLabel(" Numéro de téléphone: +514 267-6789");
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+        loginView = new LoginView();
+        registerView =  new RegisterView();
+    }
+    public void setUpMain() {
         //Setting up JFrame
-        super("Financement Automobile charExpress");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(1000, 600);
-        setVisible(true);
-        setResizable(false); // windows not resizable
-        setLocationRelativeTo(null); // to center the window in the center of the screen
+        frame.setTitle("Financement Automobile charExpress");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+        frame.setVisible(true);
+        frame.setResizable(false); // windows not resizable
+        frame.setLocationRelativeTo(null); // to center the window in the center of the screen
 
         // Set a background for JFrame
-        setContentPane(new JLabel(new ImageIcon("/Users/ksim/Desktop/autopic.jpeg")));
+        frame.setContentPane(new JLabel(new ImageIcon("./src/images/autopic.jpeg")));
         // Set some Layout
-        setLayout(new FlowLayout());
+        frame.setLayout(new FlowLayout());
 
-        //Setting up mainPanel
-        JPanel mainPanel = new JPanel();
-        //mainPanel.setBounds(40,150,220,70);
+        //Setting up mainPanel, description panel
+        mainPanel.setBounds(40,150,220,35);
         mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         mainPanel.setBackground(new Color(0,0,0,0)); // background transparent
-        add(mainPanel); //Add mainPanel to JFrame
+        frame.add(mainPanel); //Add mainPanel to JFrame
+
+        descriptionPanel.setBounds(100,250,220,35);
+        descriptionPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        descriptionPanel.setBackground(new Color(0,0,0,65)); // background transparent
+        frame.add( descriptionPanel);
 
         // Setting up 'Connexion' button
-        JButton loginButton = new JButton("Connexion");
         mainPanel.add(loginButton); //Add button to the main panel
 
         // Setting up 'Inscription' button
-        JButton registerButton = new JButton("Inscription");
         mainPanel.add(registerButton); //Add button to the main panel
 
-        // Set up
+        // Setting up descriptionLabel
+        descriptionLabel.setBounds(40,250,220,35);
+        descriptionPanel.add(descriptionLabel);
+
+        // Set up JLabel contact Label
+        contactLabel.setBounds(100,400,220,35);
+        mainPanel.add(contactLabel);
+
         cardPanel.add(mainPanel, "Main");
         //cardPanel.add(loginView, "Login");
         //cardPanel.add(registerView, "Register");
 
-        add(cardPanel);
+        frame.add(cardPanel);
 
         loginButton.addActionListener(new ActionListener() {
             @Override
@@ -70,7 +101,8 @@ public class Main extends JFrame {
         Runnable initFrame = new Runnable(){
             @Override
             public void run(){
-                new Main();
+                Main start = new Main();
+                start.setUpMain();
             }
         };
 
