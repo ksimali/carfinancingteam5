21774be8167d034daf64a975f5e5ad7c911a4630
ksimali <cassim.ali@outlook.fr>
2024-02-26 18:00:55 -0500

    Update object model: -Update User Class  -Update Client class  -Update Investor Class

--- src/model/Investor.java
+++ src/model/Investor.java
@@ -4,6 +4,8 @@ public class Investor extends User {
     // Attributes
     private String bankName;
     private char bankAccount;
+    private String riskLevel;
+    private String financialEducation;
 
     // Getters
     public String getBankName(){
@@ -13,6 +15,12 @@ public class Investor extends User {
     public char getBankAccount() {
         return bankAccount;
     }
+    public String getRiskLevel(){
+        return riskLevel;
+    }
+    public String getFinancialEducation(){
+        return financialEducation;
+    }
     // Setters
     public void setBankName(String bankName) {
         this.bankName = bankName;
@@ -20,18 +28,25 @@ public class Investor extends User {
     public void setBankAccount(char bankAccount){
         this.bankAccount = bankAccount;
     }
-
+    public void setRiskLevel(String riskLevel){
+        this.riskLevel = riskLevel;
+    }
+    public void setInvestmentLiteracy(String investmentLiteracy){
+        this.financialEducation = financialEducation;
+    }
     // Constructor
-    public Investor(String name, String email, String password, String phone, String bankName, char bankAccount){
+    public Investor(String name, String email, String password, String phone, String bankName, char bankAccount, String riskLevel,String financialEducation){
         super(name,email,password,phone);
         this.bankName = bankName;
         this.bankAccount = bankAccount;
+        this.riskLevel = riskLevel;
+        this.financialEducation = financialEducation;
     }
 
     // Methods
     @Override
     public void afficherDetails(){
         super.afficherDetails();
-        System.out.println("Nom_Banque: "+bankName+"\n"+"Compte bancaire: "+ bankAccount);
+        System.out.println("Nom_Banque: "+bankName+"\n"+"Compte bancaire: "+ bankAccount+"\n"+"Niveau_risque: "+riskLevel+"\n"+"Education financière: "+ financialEducation);
     }
 }
