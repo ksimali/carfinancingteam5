3826662ae5884a8dee752e80549e1c965c364b98
ksimali <cassim.ali@outlook.fr>
2024-02-26 11:39:11 -0500

    Set up Objet Model: -Set up parent User Class -Set up child Client Class -Set up child Investor class

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,37 @@
 package model;
 
-public class Client {
+import java.math.BigDecimal;
+
+public class Client extends User {
+
+    // Attributes
+    private String job;
+    private BigDecimal income;
+
+    // Getters
+    public String getJob() {
+        return job;
+    }
+    public BigDecimal getIncome(){
+        return income;
+    }
+    // Setters
+    public void setJob(String job) {
+        this.job = job;
+    }
+    public void setIncome(BigDecimal income){
+        this.income = income;
+    }
+
+    // Constructor
+    public Client(String name, String email, String password, String phone, String job, BigDecimal income){
+        super(name, email, password, phone);
+        this.job = job;
+        this.income = income;
+    }
+    // Methods
+    public void afficherDetails(){
+        super.afficherDetails();
+        System.out.println("job: " + job +"\n"+ "income: "+income);
+    }
 }
